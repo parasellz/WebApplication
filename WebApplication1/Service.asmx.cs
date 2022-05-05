5b6271ac63533c41cca850e2367675fef302ccc9
Parasell1 <chalermpon@aisoftthailand.com>
2022-05-05 18:27:29 +0700

    Add project files.

---
+++ WebApplication1/Service.asmx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace WebApplication1
+{
+    /// <summary>
+    /// Summary description for Service
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class Service : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+        [WebMethod]
+        public string test1(FlightMatrixService.FlightMatrixRQ request)
+        {
+            return "Hello World";
+        }
+
+
+        [WebMethod]
+        public string test2(AirSellService.OTA_AirPriceRQ request)
+        {
+            return "Hello World";
+        }
+    }
+}
